b0f78f66fab152ff53df1f13b86324d48ce19132
Testingskills <akhilrana690041@gmail.com>
2024-12-10 10:52:37 +0530

    Add files via upload

---
+++ download_files.py
@@ -0,0 +1,5 @@
+import gdown
+
+gdown.download(id='1mn1KfzLOWkoRH76k-DQNS4nHB6gsUu3R', output='last.pt')
+gdown.download(id='1UFHuKnBejgREzCJUsiVlGiYnzj7_6O-b',output='bdd100k_labels_images_train.json')
+gdown.download(id='1NyvzRCVXQJNbDDp09SvOFTV_z9BxjqGI', output = 'bdd100k_labels_images_val.json')
