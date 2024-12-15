f87e80ef95e50b2e50dbd0328f62c22d5f2a511a
Akhil Rana <rana@Akhils-Mac-mini.local>
2024-12-15 22:50:02 +0530

    gfrive id changed

--- download_files.py
+++ download_files.py
@@ -1,5 +1,5 @@
 import gdown
 
 gdown.download(id='1mn1KfzLOWkoRH76k-DQNS4nHB6gsUu3R', output='yolov5.pt')
-gdown.download(id='1UFHuKnBejgREzCJUsiVlGiYnzj7_6O-b',output='bdd100k_labels_images_train.json')
-gdown.download(id='1NyvzRCVXQJNbDDp09SvOFTV_z9BxjqGI', output = 'bdd100k_labels_images_val.json')
+gdown.download(id='1EgLYXeTtOdGyTLKfBExVUoLf8HTQyTUJ',output='bdd100k_labels_images_train.json')
+gdown.download(id='1Tj8ZnC_NpPc-3I_8tGz30HN_qOUVc9MO', output = 'bdd100k_labels_images_val.json')
